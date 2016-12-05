000b715f8c436355783e44f8131bb04c96bd7a27
Shahryar Minhas <s7.minhas@gmail.com>
2016-12-05 18:42:02 -0500

    non lol-approx c++ fans prepped

--- src/missing.cpp
+++ src/missing.cpp
@@ -1,49 +1,36 @@
-c.missing <-  cxxfunction(
-  signature(Data="numeric",DIMS="integer",MMM="integer",Yy="numeric",Ttt="integer",
-            BETAIN="numeric",BETAOUT="numeric",WW="numeric"),
-  body='
-/*Dims is c(n,p,TT,K); 
-  Z nxT; X pxTxn; Y pxpxT; 
-  */
-  Rcpp::IntegerVector dims(DIMS);
-  int ttt = Rcpp::as<int>(Ttt)-1;
-  
-  Rcpp::NumericVector Xvec(Data);
-  arma::cube X(Xvec.begin(),dims[1],dims[2],dims[0]);
-  
-  Rcpp::IntegerVector MM(MMM);
-  Rcpp::NumericVector YY(Yy);
-  arma::cube Y(YY.begin(),dims[0],dims[0],dims[2]);
-  
-  double BIN = Rcpp::as<double>(BETAIN);
-  double BOUT = Rcpp::as<double>(BETAOUT);
-  
-  arma::colvec ww = Rcpp::as<arma::colvec>(WW);
-  
+#include <RcppArmadillo.h>
+using namespace arma; 
+using namespace Rcpp; 
+
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::export]]
+
+arma::cube cMissing(
+  arma::cube X, arma::vec dims, arma::vec MM, 
+  arma::cube Y, int ttt, 
+  double BIN, double BOUT, arma::colvec ww
+  ) {
+
+  int ttt = ttt-1;  
   double dx=0, uu=0, Prob=0;
   arma::mat insides = arma::zeros(1,1);
   arma::colvec muit = arma::zeros(dims[1],1);
   
   /*---------------------------------------*/
   
-  for(int i = 0; i < dims(0); i++)
-{
-  if(std::find(MM.begin(),MM.end(),i) !=MM.end())
-{
-  for(int j = 0; j < dims(0); j++)
-{
-  if(i != j)
-{
-  dx = arma::norm(X.slice(i).col(ttt)-X.slice(j).col(ttt),2);
-  Prob = BIN*(1-dx/ww(j))+BOUT*(1-dx/ww(i));
-  Prob = 1/(1+exp(-Prob));
-  uu= arma::randu();
-  if(uu<Prob)
-{
-  Y(i,j,ttt) = 1;
-}else{
-  Y(i,j,ttt) = 0;
-}
+  for(int i = 0; i < dims(0); i++) {
+		if(std::find(MM.begin(),MM.end(),i) !=MM.end()) {
+			for(int j = 0; j < dims(0); j++) {
+				if(i != j) {
+					dx = arma::norm(X.slice(i).col(ttt)-X.slice(j).col(ttt),2);
+					Prob = BIN*(1-dx/ww(j))+BOUT*(1-dx/ww(i));
+					Prob = 1/(1+exp(-Prob));
+					uu= arma::randu();
+					if(uu<Prob) {
+						Y(i,j,ttt) = 1;
+					} else {
+						Y(i,j,ttt) = 0;
+					}
   /*  if(std::find(MM.begin(),MM.end(),j) ==MM.end())
 {
   Prob = BIN*(1-dx/ww(i))+BOUT*(1-dx/ww(j));
@@ -57,11 +44,10 @@ c.missing <-  cxxfunction(
 }
 }*/
   
-}
-}
-}
-}
+				}
+			}
+		}
+	}
   
-  return Rcpp::wrap(Y);
-  
-  ', plugin="RcppArmadillo")
+  return(Y);
+}
--- src/t2s2Parms.cpp
+++ src/t2s2Parms.cpp
@@ -1,18 +1,16 @@
-c.t2s2Parms <-  cxxfunction(
-  signature(DATA="numeric",DIMS="integer",THETAT="numeric",
-            THETAS="numeric",PHIT="numeric",PHIS="numeric"),
-  body='
-// Dims is c(n,p,TT,K)
-  Rcpp::IntegerVector dims(DIMS);
-  
-  Rcpp::NumericVector Xvec(DATA);
-  arma::cube X(Xvec.begin(),dims[1],dims[2],dims[0]);
-  double thetaT=Rcpp::as<double>(THETAT);
-  double phiT=Rcpp::as<double>(PHIT);
-  double thetaS=Rcpp::as<double>(THETAS);
-  double phiS=Rcpp::as<double>(PHIS);
-  double shapeT=0, scaleT=0, shapeS=0, scaleS=0;
-  
+#include <RcppArmadillo.h>
+using namespace arma; 
+using namespace Rcpp; 
+
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::export]]
+
+List cT2s2Parms(
+	arma::cube X, arma::vec, dims, double thetaT, 
+	double thetaS, double phiT, double phiS 
+	) {
+	
+  double shapeT=0, scaleT=0, shapeS=0, scaleS=0;  
   arma::mat insides = arma::zeros(1,1);
   
   //---------------------------------------
@@ -21,19 +19,17 @@ c.t2s2Parms <-  cxxfunction(
   scaleT = phiT;
   shapeS = thetaS + 0.5*dims(0)*dims(1)*(dims(2)-1);
   scaleS = phiS;
-  for(int i =0;i<dims(0);i++)
-{
-  insides = 0.5*trans(X.slice(i).col(0))*(X.slice(i).col(0));
-  scaleT += insides(0,0);
-  for(int tt=1;tt<dims(2);tt++)
-{
-  insides = 0.5*trans(X.slice(i).col(tt)-X.slice(i).col(tt-1))*
-  (X.slice(i).col(tt)-X.slice(i).col(tt-1));
-  scaleS += insides(0,0);
-}
-}
+  for(int i =0;i<dims(0);i++) {
+	  insides = 0.5*trans(X.slice(i).col(0))*(X.slice(i).col(0));
+	  scaleT += insides(0,0);
+	  for(int tt=1;tt<dims(2);tt++) {
+		  insides = 0.5*trans(X.slice(i).col(tt)-X.slice(i).col(tt-1))*
+		  (X.slice(i).col(tt)-X.slice(i).col(tt-1));
+		  scaleS += insides(0,0);
+		}
+	}
   
   
-  return Rcpp::List::create(shapeT,scaleT,shapeS,scaleS);
-  
-  ', plugin="RcppArmadillo")
+ return(Rcpp::List::create(shapeT,scaleT,shapeS,scaleS));
+ }
+ 
